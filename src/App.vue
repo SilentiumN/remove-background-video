aec3c96f08dad719a3d3cafc20836313dfd2243e
Anastasiia Morozova <37775903+SilentiumN@users.noreply.github.com>
2023-06-30 08:03:49 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Draw from "./components/draw.vue";
+</script>
+
+<template>
+  <main>
+    <Draw/>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/draw.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import {ref, onMounted} from 'vue';
+import { SelfieSegmentation } from "@mediapipe/selfie_segmentation";
+
+const inputVideoRef = ref(null);
+const canvasRef = ref(null);
+const contextRef = ref(null);
+
+onMounted(() => {
+  contextRef.value = canvasRef.value.getContext("2d");
+  const constraints = {
+    video: { width: { min: 1280 }, height: { min: 720 } },
+  };
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    inputVideoRef.value.srcObject = stream;
+    sendToMediaPipe();
+  });
+
+  const selfieSegmentation = new SelfieSegmentation({
+    locateFile: (file) =>
+        `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`,
+  });
+
+  selfieSegmentation.setOptions({
+    modelSelection: 1,
+    selfieMode: false,
+  });
+
+  selfieSegmentation.onResults(onResults);
+
+  const sendToMediaPipe = async () => {
+    if (!inputVideoRef.value.videoWidth) {
+      console.log(inputVideoRef.value.videoWidth);
+      requestAnimationFrame(sendToMediaPipe);
+    } else {
+      await selfieSegmentation.send({ image: inputVideoRef.value });
+      requestAnimationFrame(sendToMediaPipe);
+    }
+  };
+})
+
+const onResults = (results) => {
+
+  contextRef.value.save();
+  contextRef.value.clearRect(
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height
+  );
+  contextRef.value.drawImage(
+      results.segmentationMask,
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height
+  );
+  // Only overwrite existing pixels.
+  contextRef.value.globalCompositeOperation = "source-out";
+  contextRef.value.fillStyle = "#000";
+  /*contextRef.value.drawImage(
+      inputVideoRef.value,
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height)*/
+  contextRef.value.fillStyle = "#00FF00";
+  contextRef.value.fillRect(
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height
+  );
+
+  // Only overwrite missing pixels.
+  contextRef.value.globalCompositeOperation = "destination-atop";
+  contextRef.value.drawImage(
+      results.image,
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height
+  );
+
+  contextRef.value.restore();
+};
+
+</script>
+
+<template>
+  <div class="App">
+    <video autoPlay ref="inputVideoRef" />
+    <canvas ref="canvasRef" width="640" height="360" />
+  </div>
+</template>
+
+<style scoped>
+video {
+  width: 640px;
+  height: 360px;
+}
+</style>
